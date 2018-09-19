3dad818051bc73d816c3c056e952025e45acce3b
Badrik Modi <badrikmodi@gmail.com>
2018-09-18 22:02:35 -0700

    Master commit

---
+++ file_master.py
@@ -0,0 +1,9 @@
+import json
+
+with open('orgs.json', 'r') as fp:
+    obj = json.load(fp)
+    print(obj,'obj')
+    print('---------------------')
+    print(type(obj),'---type----')
+    x = obj[0]
+    print(x,'x')
